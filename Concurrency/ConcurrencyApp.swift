d4bbb7fb8ca7025cc386b1070a3711b9fce00514
Michel Andre Pellegrin Quiroz <michelpellegrin@10-20-56-124.0200.fed.it.iosda.org>
2024-04-04 14:48:21 +0200

    Initial commit

---
+++ Concurrency/ConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcurrencyApp.swift
+//  Concurrency
+//
+//  Created by Michel Andre Pellegrin Quiroz on 04/04/24.
+//
+
+import SwiftUI
+
+@main
+struct ConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Concurrency/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Concurrency
+//
+//  Created by Michel Andre Pellegrin Quiroz on 04/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
